5814bec3953e1a341b8dc265cf99b48d9e1b39ac
Le Nhat Hung <Le.nhthung@gmail.com>
2019-02-03 22:58:13 -0500

    Finish local beam search

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
+import pickle
 import numpy as np
 import pandas as pd
 import plotly.graph_objs as go
 import plotly.plotly as py
 import cufflinks
 from plotly.offline import iplot
+from pathlib import Path
 from models import HillClimbing as Hill
 from models import LocalBeamSearch as Beam
 
 cufflinks.go_offline()
+project_dir = Path(__file__).resolve().parents[0]
 
 def main():
     stepsizes  = [0.01, 0.05, 0.1, 0.2]
@@ -17,52 +20,57 @@ def main():
     hill_1, hill_2 = Hill(f1, stepsizes), Hill(f2, stepsizes)
     beam_1, beam_2 = Beam(f1, beam_widths), Beam(f2, beam_widths)
 
-    hill_1.start(low, high)
-    hill_2.start(low, high)
+    #hill_1.start(low, high)
+    #hill_2.start(low, high)
 
-    #report_hill(hill_1)
-    #report_hill(hill_2)
+    #report(hill_1)
+    #report(hill_2)
 
     stepsize_f1 = 0.1
     stepsize_f2 = 0.01
+    repeat = 100
 
-    beam_1.start(low, high, stepsize_f1)
-    beam_2.start(low, high, stepsize_f2)
+    beam_1.start(low, high, stepsize_f1, repeat)
+    beam_2.start(low, high, stepsize_f2, repeat)
 
-def report_beam(beam):
-    pass
+    df1 = report(beam_1)
+    df2 = report(beam_2)
 
-def table_beam(df):
-    pass
+    pickle.dump(df1, open(project_dir / 'data' / '3b_f1_df.pkl', 'wb'))
+    pickle.dump(df2, open(project_dir / 'data' / '3b_f2_df.pkl', 'wb'))
 
-def plot_beam(df):
-    pass
+def report(localSearch):
+    stats = {
+        'f value mean': localSearch.mean_f_values(),
+        'f value standard deviation': localSearch.std_f_values(),
+        'num steps mean': localSearch.mean_num_steps(),
+        'num steps standard deviation': localSearch.std_num_steps(),
+    }
+    if isinstance(localSearch, Hill):
+        stats['step size'] = localSearch.stepsizes
+    else:
+        stats['beam width'] = localSearch.beam_widths
 
-def report_hill(hill):
-    # Courtesy of Wolfram
-    f1_max = 2
+    df = pd.DataFrame(stats)
 
-    df = pd.DataFrame({
-        'step size': hill.stepsizes,
-        'f value mean': hill.mean_f_values(),
-        'f value standard deviation': hill.std_f_values(),
-        'num steps mean': hill.mean_num_steps(),
-        'num steps standard deviation': hill.std_num_steps(),
-    })
+    return df
 
-    table_hill(df)
-    plot_hill(df)
+    #table(localSearch, df)
+    #plot(localSearch, df)
 
-def table_hill(df):
+def table(localSearch, df):
+    x_label = 'Step size' if isinstance(localSearch, Hill) else 'Beam width'
+    x_val = df[x_label.lower()]
+
     trace = go.Table(
-        header=dict(values=['Step size',
-                            'Mean f value', 'f value std deviation ',
+        header=dict(values=[x_label,
+                            'Mean f max', 'f max std deviation ',
                             'Mean num steps', 'num steps std deviation'],
                     line = dict(color='#7D7F80'),
                     fill = dict(color='#a1c3d1'),
                     align = ['left'] * 5
         ),
-        cells=dict(values=[df['step size'],
+        cells=dict(values=[x_val,
                             df['f value mean'],
                             df['f value standard deviation'],
                             df['num steps mean'],
@@ -72,24 +80,24 @@ def table_hill(df):
                     align = ['left'] * 5
         )
     )
-
     data = [trace]
     fig = dict(data=data)
     iplot(fig)
 
-def plot_hill(df):
-    df = df.set_index('step size')
+def plot(localSearch, df):
+    x_label = 'Step size' if isinstance(localSearch, Hill) else 'Beam width'
+    df = df.set_index(x_label.lower())
 
     df['f value standard deviation'].iplot(
         mode='lines+markers',
-        xTitle='Step size',
-        title='f value standard deviation against step size'
+        xTitle=x_label,
+        title='f max standard deviation against %s' % x_label.lower()
     )
 
     df['f value mean'].iplot(
         mode='lines+markers',
-        xTitle='Step size',
-        title='f value mean against step size'
+        xTitle=x_label,
+        title='Mean f max against %s' % x_label.lower()
     )
 
 def f1(x, y):
--- models.py
+++ models.py
@@ -6,6 +6,21 @@ class LocalSearch:
         self.num_steps = [[] for _ in range(len(specs))]
         self.f_values = [[] for _ in range(len(specs))]
 
+    def get_neighbours(self, xy, low_high, stepsize):
+        x, y = xy
+        low, high = low_high
+        return (
+            (x, min(y + stepsize, high)), # top
+            (x, max(y - stepsize, low)), # bottom
+            (max(x - stepsize, low), y), # left
+            (min(x + stepsize, high), y), # right
+
+            (min(x + stepsize, high), min(y + stepsize, high)), # top-right
+            (min(x + stepsize, high), max(y - stepsize, low)), # bottom-right
+            (max(x - stepsize, low), max(y - stepsize, low)), # bottom-left
+            (max(x - stepsize, low), min(y + stepsize, high)), # top-left
+        )
+
     def get_xy_0(self, low, high, size):
         return np.random.uniform(low=low, high=high, size=(size, 2)).tolist()
 
@@ -48,53 +63,39 @@ class HillClimbing(LocalSearch):
             xy_0_list = self.get_xy_0(low=low, high=high, size=100)
 
             for x0, y0 in xy_0_list:
-                f_val, num_steps = self.hillclimb(
-                    xy_0=(x0, y0),
-                    low_high=(low, high),
-                    stepsize=stepsize)
+                x, y = x0, y0
+                cur_val = self.f(x, y)
+                num_steps = 1
+
+                while True:
+                    next_val, (xi, yi) = self.hillclimb(
+                        x=x,
+                        y=y,
+                        low_high=(low, high),
+                        stepsize=stepsize)
+
+                    if next_val <= cur_val:
+                        break
+                    else:
+                        x, y = xi, yi
+                        cur_val = next_val
+                        num_steps += 1
 
                 self.save_num_steps(num_steps, i)
-                self.save_f_value(f_val, i)
+                self.save_f_value(cur_val, i)
 
-    def hillclimb(self, xy_0, low_high, stepsize):
+    def hillclimb(self, x, y, low_high, stepsize):
         low, high = low_high
-        x, y = xy_0
-        cur_val = self.f(x, y)
-        num_steps = 1
 
-        while True:
-            neighbours = self.get_neighbours(
-                xy=(x, y),
-                low_high=(low, high),
-                stepsize=stepsize)
+        neighbours = self.get_neighbours(
+            xy=(x, y),
+            low_high=(low, high),
+            stepsize=stepsize)
 
-            vals = [self.f(x, y) for x, y in neighbours]
-            next_val = max(vals)
-            xi, yi = neighbours[vals.index(next_val)]
-
-            if next_val <= cur_val:
-                break
-            else:
-                x, y = xi, yi
-                cur_val = next_val
-                num_steps += 1
-
-        return cur_val, num_steps
-
-    def get_neighbours(self, xy, low_high, stepsize):
-        x, y = xy
-        low, high = low_high
-        return (
-            (x, min(y + stepsize, high)), # top
-            (x, max(y - stepsize, low)), # bottom
-            (max(x - stepsize, low), y), # left
-            (min(x + stepsize, high), y), # right
-
-            (min(x + stepsize, high), min(y + stepsize, high)), # top-right
-            (min(x + stepsize, high), max(y - stepsize, low)), # bottom-right
-            (max(x - stepsize, low), max(y - stepsize, low)), # bottom-left
-            (max(x - stepsize, low), min(y + stepsize, high)), # top-left
-        )
+        vals = [self.f(x, y) for x, y in neighbours]
+        next_val = max(vals)
+        xi, yi = neighbours[vals.index(next_val)]
+        return next_val, (xi, yi)
 
 
 class LocalBeamSearch(LocalSearch):
@@ -105,7 +106,45 @@ class LocalBeamSearch(LocalSearch):
         self.beam_widths = beam_widths
         super().__init__(f, beam_widths)
 
-    def start(self, low, high, stepsize):
+    def start(self, low, high, stepsize, repeat):
         for i, beam_width in enumerate(self.beam_widths):
-            for _ in range(100):
+            for _ in range(repeat):
                 xy_0_list = self.get_xy_0(low=low, high=high, size=beam_width)
+                f_val, num_steps = self.beamsearch(
+                    xy_0_list=xy_0_list,
+                    beam_width=beam_width,
+                    low_high=(low, high),
+                    stepsize=stepsize)
+
+                self.save_num_steps(num_steps, i)
+                self.save_f_value(f_val, i)
+
+    def beamsearch(self, xy_0_list, beam_width, low_high, stepsize):
+        beam = [(x, y, self.f(x, y)) for x, y in xy_0_list]
+        num_steps = 1
+
+        while True:
+            #print('beam $d: ' % num_steps, end=''); print(beam)
+            cur_vals = [f for x, y, f in beam]
+            neighbours = [
+                neighbour
+                for x, y, f in beam
+                for neighbour in self.get_neighbours((x, y), low_high, stepsize)
+            ]
+            next_states = [(x, y, self.f(x, y)) for x, y in neighbours]
+            k_best = sorted(
+                beam + next_states,
+                key=lambda tup: tup[2],
+                reverse=True
+            )[:beam_width]
+            k_best_vals = [f for x, y, f in k_best]
+
+            print(k_best_vals)
+            print(cur_vals)
+            if max(k_best_vals) <= min(cur_vals):
+                break
+            else:
+                beam = k_best
+                num_steps += 1
+
+        return max(cur_vals), num_steps
